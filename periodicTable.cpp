2ad9ac8208b090a9ac4c9eff266c0a0d5a1fb0b8
Bitsu Alemayehu <BitsuA.Mamo@gmail.com>
2020-08-27 23:25:13 +0300

    Finished the search algorithm

--- periodicTable.cpp
+++ periodicTable.cpp
@@ -11,16 +11,16 @@ const string RESET = "\033[0m";
 const string BOLDWHITE = "\033[1m\033[37m";
 
 // Array constants for colors and classification
-const string LAT[2] = {"\033[31m", "Lanthanide"};					  /* Red */
-const string ALKMETAL[2] = {"\033[32m", "Alkali Metal"};			  /* Green */
-const string ALKEARTH[2] = {"\033[34m", "Alkali Earth"};			  /* Blue */
-const string HALOGEN[2] = {"\033[35m", "Halogen"};					  /* Magenta */
-const string NOBGAS[2] = {"\033[36m", "Noble Gas"};					  /* Cyan */
-const string ACT[2] = {"\033[1m\033[31m", "Actinide"};				  /* Bold LAT */
-const string TRANSMETAL[2] = {"\033[1m\033[32m", "Transition Metal"}; /* Bold ALKMETAL */
-const string BASICMETAL[2] = {"\033[1m\033[34m", "Basic Metal"};	  /* Bold ALKEARTH */
-const string NONMETAL[2] = {"\033[1m\033[35m", "Non Metal"};		  /* Bold MAGENTA */
-const string METALLOID[2] = {"\033[1m\033[36m", "Metalloid"};		  /* Bold Cyan */
+const string LAT[2] = {"\033[31m", "Lanthanide"};
+const string ALKMETAL[2] = {"\033[32m", "Alkali Metal"};
+const string ALKEARTH[2] = {"\033[34m", "Alkali Earth"};
+const string HALOGEN[2] = {"\033[35m", "Halogen"};	
+const string NOBGAS[2] = {"\033[36m", "Noble Gas"};
+const string ACT[2] = {"\033[1m\033[31m", "Actinide"};
+const string TRANSMETAL[2] = {"\033[1m\033[32m", "Transition Metal"};
+const string BASICMETAL[2] = {"\033[1m\033[34m", "Basic Metal"};	
+const string NONMETAL[2] = {"\033[1m\033[35m", "Non Metal"};	
+const string METALLOID[2] = {"\033[1m\033[36m", "Metalloid"};
 
 struct element{
     char name[14];
@@ -32,28 +32,46 @@ struct element{
 
 void mainMenu();
 void periodicTable();
-void searchRes(string nameSymbol);
-void searchRes(int atomicNumber);
+void searchRes(element periodicTable[][COLS], string nameSymbol);
+void searchRes(element periodicTable[][COLS], int atomicNumber);
 string calcElecConfig(int atomicNumber);
+void viewElement(element periodicTable, int r, int c);
+void noElement();
 
 int main(){
     element periodicTable =  
-    int input;
+    int choice;
     do{
         mainMenu();
-        cin>>input;
-        switch(input){
+        cin>>choice;
+        switch(choice){
             case 1:
                 // Gets periodicTable;
                 periodicTable();
                 break;
             case 2:
-                //TODO: Research how to get value of any type auto
-                // Gets search result of a specific target;
-                cout<<"Input search target(Name, Symbol, Atomic Number)";
-                //auto target = cin;
-                //cin>>target;
-                //searchRes(target);
+                subMenu();
+                cin>>subChoice;
+                switch(subChoice){
+                    case 1:
+                        char target[14];
+                        cout<<"Input: ";
+                        cin>>target;
+                        searchRes(periodicTable, target);
+
+                        break;
+                    case 2:
+                        int target;
+                        cout<<"Input: ";
+                        cin>>target;
+                        searchRes(periodicTable, target);
+        
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
                 break;
         }
     }while((input < 1 || input > 2) & input  != 0);
@@ -73,10 +91,46 @@ void mainMenu(){
 	cout << setw(1) << "|" << "Choice" << setw(5) << " |";
 }// Already did it
 
-void periodicTable(){} // Did it but need some revision maybe an overhall
 
-void searchRes(int atomicNumber){} 
+void searchRes(element periodicTable[][COLS]int atomicNumber){
+    bool exists = false;
+    for(int r = 0; r < ROWS; ++r){
+        for(int c = 0; c < COLS; ++C){
+            if(atomicNumber == periodicTable[r][c].atomicNumber){
+                viewElement(periodicTable, r, c);
+                exists = true;
+            }
+        }
+    }
+    if(!exists){
+       noElement(); 
+    }
+} 
 
-void searchRes(char nameSymbol){}
+void searchRes(element periodicTable[][COLS], char target[]){
+    bool exists = false;
+    for(int r = 0; r < ROWS; ++r){
+        for(int c = 0; c < COLS; ++C){
+            if(target == periodicTable[r][c].symbol || target == periodicTable[r][c].name){
+                viewElement(periodicTable, r, c);
+                exists = true;
+            }
+        }
+    }
+    if(!exists){
+       noElement(); 
+    }
+}
+//Copy form previous code
+string calcElecConfig(int atomicNumber){
+    string config = "";
+    if(atomicNumber <= 2){
+        config += atomicNumber;
+    }
+} // Already did it
+//This is for output so can do it later
+void viewElement(element periodicTable[][COLS], int r, int c){}
 
-string calcElecConfig(int atomicNumber){} // Already did it
+void noElement(){}
+
+void printTable(){} // Did it but need some revision maybe an overhall
